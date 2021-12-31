6cd0e94d782b3922eacd0da2543f695c3d833291
Şükrü <kurt12118@gmail.com>
2021-12-31 17:25:57 +0300

    Add files via upload

---
+++ SKR_Renderer/Library/SKR_Renderer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <SDL.h>
+
+typedef struct {
+	float x;
+	float y;
+	float w;
+	float h;
+}SKR_Rect;
+
+typedef struct SKR_RenderObject SKR_RenderObject;
+
+typedef struct SKR_RenderAnimationObject SKR_RenderAnimationObject;
+
+void SKR_RendererInit();
+
+void SKR_RendererQuit();
+
+SKR_RenderObject* SKR_CreateObjectbyPNG(char* PNGfilepath, SKR_Rect* desrect, SKR_Rect* srcrect, int animation, int milliseconds, int* condition);
+
+SKR_RenderObject* SKR_CreateObjectbyColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a, SKR_Rect* desrect, int animation, int milliseconds, int* condition);
+
+SKR_RenderObject* SKR_CreateObjectbyTTF(char* fontpath, int size, char* text, SKR_Rect* desrect, SKR_Rect* srcrect, Uint8 r, Uint8 g, Uint8 b, Uint8 a, int animation, int milliseconds, int* condition);
+
+SKR_RenderObject* SKR_CreateObjectbyTexture(SDL_Texture* texture, SKR_Rect* desrect, SKR_Rect* srcrect, int animation, int milliseconds, int* condition);
+
+void SKR_ChangeObjectAlpha(SKR_RenderObject* RenderObject, Uint8 a);
+
+SDL_Texture* SKR_GetTexture(SKR_RenderObject* RenderObject);
+
+void SKR_DestroyObject(SKR_RenderObject* RenderObject);
+
+void SKR_DestroyAllObjects();
+
+void SKR_RenderAll();
