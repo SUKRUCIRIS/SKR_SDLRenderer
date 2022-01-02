efbd3a99005be7fc3c5216f66ffeccbc451c97bf
Şükrü <kurt12118@gmail.com>
2022-01-02 14:47:04 +0300

    Update SKR_Renderer.h

--- SKR_Renderer/Library/SKR_Renderer.h
+++ SKR_Renderer/Library/SKR_Renderer.h
@@ -8,11 +8,13 @@ typedef struct {
 	float h;
 }SKR_Rect;
 
+//you should look at the example to see how you can use these functions
+
 typedef struct SKR_RenderObject SKR_RenderObject;
 
-void SKR_RendererInit();
+void SKR_RendererInit();//this function starts sdl libraries and skr, you must call this function before all of the other functions
 
-void SKR_RendererQuit();
+void SKR_RendererQuit();//this function quits sdl libraries and skr, also frees all the memory which is retained by these libraries
 
 SKR_RenderObject* SKR_CreateObjectbyPNG(char* PNGfilepath, SKR_Rect* desrect, SKR_Rect* srcrect, int animation, int milliseconds, int* condition);
 
@@ -26,8 +28,8 @@ void SKR_ChangeObjectAlpha(SKR_RenderObject* RenderObject, Uint8 a);
 
 SDL_Texture* SKR_GetTexture(SKR_RenderObject* RenderObject);
 
-void SKR_DestroyObject(SKR_RenderObject* RenderObject);
+void SKR_DestroyObject(SKR_RenderObject* RenderObject);//free the memory which is retained by one SKR_RenderObject
 
-void SKR_DestroyAllObjects();
+void SKR_DestroyAllObjects();//frees all the memory which is retained by SKR_RenderObjects
 
-void SKR_RenderAll();
+void SKR_RenderAll();//renders all SKR_RenderObjects
